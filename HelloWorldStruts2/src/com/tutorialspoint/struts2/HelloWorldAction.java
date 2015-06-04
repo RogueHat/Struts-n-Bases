272227d414874f80620418f4993d797d39703c8f
carlo <carlomiguelh@gmail.com>
2015-06-03 20:34:08 -0500

    Password-y function

--- HelloWorldStruts2/src/com/tutorialspoint/struts2/HelloWorldAction.java
+++ HelloWorldStruts2/src/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -1,17 +1,27 @@
 package com.tutorialspoint.struts2;
 
-public class HelloWorldAction{
-   private String name;
+import com.opensymphony.xwork2.ActionSupport;
 
-   public String execute() throws Exception {
-      return "success";
-   }
-   
-   public String getName() {
-      return name;
-   }
+public class HelloWorldAction extends ActionSupport {
+	private String name;
 
-   public void setName(String name) {
-      this.name = name;
-   }
+//	public String execute() throws Exception {
+//		return "success";
+//	}
+
+	public String execute() throws Exception {
+		if (name.equals("SECRET")){
+			return SUCCESS;
+		}else{
+			return ERROR;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
